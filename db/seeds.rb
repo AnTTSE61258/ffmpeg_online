3f3278bfc04b0ebe9603bd0ddea0212205cdcf86
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2017-09-27 20:40:09 +0700

    Add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Format.create(name: 'mpeg', description: 'MPEG-1 Systems / MPEG program stream')
+Format.create(name: 'mp4', description: 'MP4 (MPEG-4 Part 14)')
+Format.create(name: 'h264', description: 'raw H.264 video')
+Format.create(name: 'avi', description: 'AVI (Audio Video Interleaved)')
+Format.create(name: 'avi', description: 'AVI (Audio Video Interleaved)')
+Format.create(name: 'asf', description: 'ASF (Advanced / Active Streaming Format)')
+Format.create(name: 'mov', description: 'QuickTime / MOV')
+Format.create(name: 'flv', description: 'FLV (Flash Video)')

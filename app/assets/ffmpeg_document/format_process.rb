a778eaee474b2025051f9092c692dbeaf19b5f83
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2017-10-07 17:52:25 +0900

    I

--- app/assets/ffmpeg_document/format_process.rb
+++ app/assets/ffmpeg_document/format_process.rb
@@ -1,17 +1,17 @@
-Format.delete_all
-text = File.open(Dir.pwd + '/app/assets/ffmpeg_document/f.txt').read
-text.gsub!(/\r\n?/, "\n")
-result = []
-text.each_line do |line|
-  a = line.gsub(/\s+/m, ' ').strip.split(' ')
-  description = ''
-  for num in 2..a.size do
-    description << a[num].to_s + ' '
-  end
-  description.strip!
-  item = [a[1], description]
-  result << item
-  f = Format.new(name: item[0], description: item[1])
-  f.save
-end
-p result
+# Format.delete_all
+# text = File.open(Dir.pwd + '/app/assets/ffmpeg_document/f.txt').read
+# text.gsub!(/\r\n?/, "\n")
+# result = []
+# text.each_line do |line|
+#   a = line.gsub(/\s+/m, ' ').strip.split(' ')
+#   description = ''
+#   for num in 2..a.size do
+#     description << a[num].to_s + ' '
+#   end
+#   description.strip!
+#   item = [a[1], description]
+#   result << item
+#   f = Format.new(name: item[0], description: item[1])
+#   f.save
+# end
+# p result
